a7839de7a18853c3ffdf7657889f104cadfb2194
QuentinRapilly <quentin.rapilly@ensta-paris.fr>
2022-03-10 12:03:02 +0100

    [*] correction du TP5

--- Projet5/main.jl
+++ Projet5/main.jl
@@ -1,4 +1,4 @@
-using CLPLEX
+using CPLEX
 using JuMP
 
 # Paramètres du problème
@@ -7,8 +7,8 @@ M = 4 # Nombre de modes
 E = [3 for _ in 1:T] # Impact environnemental max
 f = [10, 30, 60, 90] # Couts fixes des modes de production
 e = [8, 6, 4, 2] # Impact environnementaux des modes de production
-h = ones(T) # Cout de stockage unitaire (invariable au cours du temps)
-p = zeros(T, M) # Cout d'approvisionnement unitaire (invariable au cours du temps et indépendant du mode)
+h = ones(Int, T) # Cout de stockage unitaire (invariable au cours du temps)
+p = zeros(Int, T, M) # Cout d'approvisionnement unitaire (invariable au cours du temps et indépendant du mode)
 
 
 function solve_instance(T::Int, M::Int, E::Array{Int, 1}, d::Array{Int, 1}, f::Array{Int, 1}, e::Array{Int, 1}, h::Array{Int, 1}, p::Array{Int, 2}, P::Int)
@@ -18,17 +18,20 @@ function solve_instance(T::Int, M::Int, E::Array{Int, 1}, d::Array{Int, 1}, f::A
     # Variables
     @variable(model, x[1:T, 1:M] >= 0)
     @variable(model, y[1:T, 1:M], Bin)
-    @variable(model, s[1:T] >= 0)
+    @variable(model, s[1:T+1] >= 0)
     @variable(model, pol[1:T])
 
     # Objectif
-    @objective(model, Min, sum(p[t, m]*x[t, m] + f[t]*y[t] for m in 1:M, t in 1:T) + sum(h[t]*s[t] for t in 1:T))
+    @objective(model, Min, sum(p[t, m]*x[t, m] + f[m]*y[t, m] for m in 1:M, t in 1:T) + sum(h[t]*s[t+1] for t in 1:T))
 
     # Contraintes
-    @constraint(model, [t in 1:T], sum(x[t, m] - s[t] + s[t-1] for m in 1:M) == d[t])
+    @constraint(model, s[1]==0)
+    @constraint(model, [t in 1:T], sum(x[t, m] - s[t+1] + s[t] for m in 1:M) == d[t])
     @constraint(model, [t in 1:T, m in 1:M], x[t, m] <= sum(d[t_prime] for t_prime in t:T)*y[t, m])
-    @constraint(model, [t in 1:T], sum( sum(e[t_prime, m] - E[t_prime] for m in 1:M) for t_prime in t:min(T, t+P)) == pol[t])
+    @constraint(model, [t in 1:T], sum( sum(e[m] - E[t_prime] for m in 1:M) for t_prime in t:min(T, t+P)) == pol[t])
     @constraint(model, [t in 1:T], pol[t] <= 0)
+
+    print(JuMP.constraints_string(model))
 
     optimize!(model)
     x_val = JuMP.value.(x)
@@ -49,7 +52,7 @@ nb_iter = 10
 for P in 1:T
     # Calcul du cout moyen et de la pollution moyenne sur plusieurs tirages aleatoires de demandes
     for _ in 1:nb_iter
-        d = [rand(20, 70) for _ in 1:T] # Demande suit loi uniforme
+        d = [mod(rand(Int),70-20)+20 for _ in 1:T]# Demande suit loi uniforme
         x, y, s, pol, obj = solve_instance(T, M, E, d, f, e, h, p, P)
         cout_moyen[i] += obj
         pol_moyenne[i] += sum(pol)/T
