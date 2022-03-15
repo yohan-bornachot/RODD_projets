ae21451ccc2fe3c79cd8a6f8108772ccfd96662c
QuentinRapilly <quentin.rapilly@ensta-paris.fr>
2022-03-15 15:51:46 +0100

    [*] proj 4 fini

--- Projet4/main.jl
+++ Projet4/main.jl
@@ -15,6 +15,16 @@ function print_grid(x::Array{Float64, 2})
     end
 end
 
+function create_instance(n::Int, m::Int, t_min::Int, t_max::Int)
+    t = zeros(Int,n,m)
+    for i in 1:n
+        for j in 1:m 
+            t[i,j] = mod(rand(Int),t_max-t_min)+t_min
+        end
+    end
+    return t
+end
+
 function main()
     # Inclure t
     include("ExplForet_jl.dat")
@@ -34,4 +44,59 @@ function main()
 
 end
 
-main()
+function evaluate_influence_of_n()
+    nb_cases = 10
+    n = [10+5*i for i in 0:nb_cases-1]
+
+    nb_iter = 10
+    
+    w1 = 1.
+    w2 = 1.
+
+    l = 4.
+    g = 5.
+
+    vec_time_m1 = zeros(Float64, nb_cases)
+    vec_time_m2 = zeros(Float64, nb_cases)
+    vec_obj_m1 = zeros(Float64, nb_cases)
+    vec_obj_m2 = zeros(Float64, nb_cases)
+
+    for i in 1:nb_cases
+        println("Processing n = ", n[i])
+        for _ in 1:nb_iter
+            t = create_instance(n[i],n[i],60,90)
+            start = time()
+            _,obj = methode_mixte(t,w1,w2,l,g)
+            stop = time()
+
+            vec_time_m1[i] += stop - start 
+            vec_obj_m1[i] += obj
+
+            start = time()
+            _,obj = methode_quadratique(t,w1,w2,l,g)
+            stop = time()
+
+            vec_time_m2[i] += stop - start 
+            vec_obj_m2[i] += obj
+        end
+
+        vec_obj_m1[i] = round(vec_obj_m1[i]/nb_iter, digits = 4)
+        vec_obj_m2[i] = round(vec_obj_m2[i]/nb_iter, digits = 4)
+        vec_time_m1[i] = round(vec_time_m1[i]/nb_iter, digits = 4)
+        vec_time_m2[i] = round(vec_time_m2[i]/nb_iter, digits = 4)
+    end
+
+    fp = open("res/influence_of_n.res", "w")
+
+    println(fp, n)
+    println(fp, vec_time_m1)
+    println(fp, vec_obj_m1)
+    println(fp, vec_time_m2)
+    println(fp, vec_obj_m2)
+    close(fp)
+
+end
+
+evaluate_influence_of_n()
+
+#main()
