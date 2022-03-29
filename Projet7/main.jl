8a0baaa1e23c01f145d02884208490de77b492b1
yohan.bornachot <yohan.bornachot@gmail.com>
2022-03-29 22:59:20 +0200

    [*] resolution projet 7 en cours

--- Projet7/main.jl
+++ Projet7/main.jl
@@ -2,7 +2,7 @@ using JuMP
 using CPLEX
 include("read_instance.jl")
 
-function planification_culturale(n_parcelles::Int, T::Int, lmax::Int, amax::Int, C::Array{Int, 1}, Demande::Array{Int, 2}, Sommets::Array{Int, 2}, Arcs::Array{Int, 2})
+function planification_culturale(n_parcelles::Int, T::Int, SURF::Int, lmax::Int, amax::Int, C::Array{Int, 2}, Demande::Array{Int, 2}, Sommets::Array{Sommet, 1}, Arcs::Array{Arc, 1})
     """
     n_parcelles : nombre de parcelles
     T : horizon de temps
@@ -14,9 +14,6 @@ function planification_culturale(n_parcelles::Int, T::Int, lmax::Int, amax::Int,
     Arcs : Transitions du graphe Rp(l, a, i, j) --> matrice (n_etats, n_etats)
     donnant les rendements de l'état d'arrivée (l, a, j) quand la culture précédente était i
     """
-
-    n_etats = size(Sommets, 1)
-    n_arcs = size(Arcs, 1)
 
     # Initialisation du modèle
     model = Model(CPLEX.Optimizer)
@@ -30,18 +27,18 @@ function planification_culturale(n_parcelles::Int, T::Int, lmax::Int, amax::Int,
     @variable(model, x[1:n_parcelles, 1:T, u in 1:length(Arcs)], Bin)
 
     # Objectif
-    @objective(model, Min, sum(x[p, 1, u] for p in 1:n_parcelles for u in 1:length(Arcs)))
+    sommet_init = Sommet(2, 0, 0)
+    @objective(model, Min, sum(x[p, 1, u.ind] for p in 1:n_parcelles for u in arcs_sortants(sommet_init, Arcs)))
 
     # Contraintes:
     # Respect de la demande
-    @constraint(model, [t in 1:T, c in 1:n_cultures], sum(x[p, t, u]*arc.r for arc in Arcs for p in 1:n_parcelles) >= Demande[c, t])
+    @constraint(model, [t in 1:T, c in C], sum(x[p, t, u.ind]*u.r for u in Arcs for p in 1:n_parcelles) >= Demande[c, t])
     # Initialisation du flux dans les parcelles
-    sommet_init = Sommet(2, 0, 0)
-    @constraint(model, [p in 1:n_parcelles], sum(x[p, 1, u] for u in arcs_sortants(sommet_init)) <= 1)
+    @constraint(model, [p in 1:n_parcelles], sum( x[p, 1, u.ind] for u in arcs_sortants(sommet_init, Arcs) ) <= 1)
     # Conservation du flux dans les parcelles
-    @constraint(model, [p in 1:n_parcelles, t in 1:T, sommet in Sommets], sum(x[p, t, u] for u in arcs_entrants(sommet)) == sum(x[p, t, u] for u in arcs_sortants(sommet)))
+    @constraint(model, [p in 1:n_parcelles, t in 2:T-1, sommet in Sommets], sum(x[p, t, u.ind] for u in arcs_entrants(sommet, Arcs)) == sum(x[p, t, u.ind] for u in arcs_sortants(sommet, Arcs)))
     # Impossibilité d'aller d'un sommet à un autre si paire à paire ou impaire à impaire
-    @constraint(model, [p in 1:n_parcelles, t in 1:T, u in Arcs ; u.f.j == C[1+t%2] ], x[p, t, u] == 0)
+    @constraint(model, [p in 1:n_parcelles, t in 1:T, u in Arcs ; u.j.j==C[1+t%2] ], x[p, t, u.ind] == 0)
 
     optimize!(model)
     x_val = JuMP.value.(x)
@@ -50,23 +47,32 @@ function planification_culturale(n_parcelles::Int, T::Int, lmax::Int, amax::Int,
     return x_val, obj
 end
 
-function arcs_entrants(sommet::Sommet)
-    return [arc for arc in Arcs if sommet == arc.i]
+function egalite_sommet(sommetA, sommetB)
+    return (sommetA.l == sommetB.l && sommetA.a == sommetB.a && sommetA.j == sommetB.j)
 end
 
-function arcs_sortants(sommet::Sommet)
-    return [arc for arc in Arcs if sommet == arc.j]
+function arcs_entrants(sommet::Sommet, arcs::Array{Arc, 1})
+    return [arc for arc in arcs if egalite_sommet(sommet, arc.j)]
 end
+
+function arcs_sortants(sommet::Sommet, arcs::Array{Arc, 1})
+    return [arc for arc in arcs if egalite_sommet(sommet, arc.i)]
+end
+
 
 function main()
-    filename = "CROPRODD.dat"
-    n_parcelles, T, lmax, amax, C, Demande, Sommets, Arcs = read_instance(filename)
+    filename = "modify.dat"
+    NbParcelles, T, SURF, lmax, amax, C, Demande, sommets, arcs = read_instance(filename)
 
-    x, obj = planification_culturale(n_parcelles, T, lmax, amax, C, Demande, Sommets, Arcs)
-    
+    start = time()
+    x, obj = planification_culturale(NbParcelles, T, SURF, lmax, amax, C, Demande, sommets, arcs)
+    stop = time()
+    taken_time = stop - start
     fp = open("./result.csv", "w")
-    println(fp, x)
-    println(fp, obj)
+    println(fp, "solution = ", x)
+    println(fp, "objectif = ", obj )
+    println(fp, "taken_time = ", taken_time)
     close(fp)
 end
 
+main()
--- Projet7/read_instance.jl
+++ Projet7/read_instance.jl
@@ -1,4 +1,3 @@
-
 mutable struct Sommet
     l::Int # age de la jachere
     a::Int # age de la culture
@@ -6,6 +5,7 @@ mutable struct Sommet
 end
 
 mutable struct Arc
+    ind::Int # indice de l'arc dans la liste
     i::Sommet # initial
     j::Sommet # final
     r::Int # rendement
@@ -16,14 +16,13 @@ function read_instance(name::String)
     n_sommets = size(Sommets,1)
     n_arcs = size(Arcs,1)
 
-    sommets = Vector{Sommet}(undef,n_sommets)
-    arcs = Vector{Arc}(undef,n_arcs)
+    sommets = Vector{Sommet}(undef, n_sommets)
+    arcs = Vector{Arc}(undef, n_arcs)
     for i in 1:n_sommets
         sommets[i] = Sommet(Sommets[i,1],Sommets[i,2],Sommets[i,3])
     end
     for i in 1:n_arcs
-        arcs[i] = Arc(Sommet(Arcs[i,1],Arcs[i,2],Arcs[i,3]), Sommet(Arcs[i,4],Arcs[i,5],Arcs[i,6]), Arcs[i,7])
+        arcs[i] = Arc(i, Sommet(Arcs[i,1],Arcs[i,2],Arcs[i,3]), Sommet(Arcs[i,4],Arcs[i,5],Arcs[i,6]), Arcs[i,7])
     end
-
     return NbParcelles, T, SURF, lmax, amax, C, Demande, sommets, arcs
 end
